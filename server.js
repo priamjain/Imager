1e13f288bbe68961d3d98362cd31a85bf77d8c70
Priam Jain <priamjain8@gmail.com>
2020-07-19 16:27:43 +0530

    added express server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const path = require("path");
+
+app.use(express.static(path.join(__dirname,'client','build')))
+
+app.get("*",(req,res)=>{
+	res.sendFile(path.join(__dirname,'client','build',"index.html"))
+})
+
+app.listen(preocess.env.PORT || 5000);
